fda74af7f2100498bd1a3ac7ea18ae34bb98feba
nluuarch@gmail.com <nluuarch@gmail.com>
2022-01-13 13:29:38 -0800

    commit tree 36e35e493cfff65c8036b3f617875876f33abcce

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+  def index 
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      reviews = Review.all
+    end 
+      render json: reviews, include: :user
+  end
+
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
